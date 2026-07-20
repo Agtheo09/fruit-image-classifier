94838c282657c4900967e4501ac8b85f486d773b
Agtheo09 <agtheo09@gmail.com>
2026-07-21 00:22:51 +0300

    Automatic Data logs and plots

---
+++ configs/config.py
@@ -0,0 +1,32 @@
+SEED = 42
+
+# ----------------------------------- Dataset ----------------------------------- #
+CLASSES = (
+    "apple",
+    "banana",
+    "orange",
+    "pineapple",
+    "watermelon"
+)
+
+NUM_CLASSES = 5
+
+TRAIN_CSV = "labels/train_labels.csv"
+TRAIN_IMG_DIR = "imgs/dataset/train_imgs"
+
+TEST_CSV = "labels/test_labels.csv"
+TEST_IMG_DIR = "imgs/dataset/test_imgs"
+
+# ----------------------------------- Training ----------------------------------- #
+
+BATCH_SIZE = 16
+NUM_WORKERS = 4
+EPOCHS = 2
+
+LEARNING_RATE = 0.0001
+WEIGHT_DECAY = 0.0001
+
+# ------------------------------------ Model ------------------------------------ #
+
+MODEL_NAME = "resnet18_finetuned_layer4"
+MODEL_PATH = "./models/resnet182layersUnFreezed25epochNoLabelSmoothing.pth"
